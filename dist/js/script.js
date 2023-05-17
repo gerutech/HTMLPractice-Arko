83c380075df255724533ca09b1099351cc192a80
Konstantin <80408570+gerutech@users.noreply.github.com>
2023-05-17 17:35:35 +0300

    Add files via upload

--- dist/js/script.js
+++ dist/js/script.js
@@ -1,12 +1,10 @@
 const hamburger = document.querySelector('.hamburger');
 const close = document.querySelector('.close');
 
-const navTop = document.querySelector('.header__top');
-const navBottom = document.querySelector('.header__bottom');
+const mobileMenu = document.querySelector('.mobile-menu');
 
 const mobileMenuToggle = function () {
-  navTop.classList.toggle('active');
-  navBottom.classList.toggle('active');
+  mobileMenu.classList.toggle('active');
   hamburger.classList.toggle('hidden');
   close.classList.toggle('hidden');
 };
@@ -17,4 +15,23 @@ hamburger.addEventListener('click', () => {
 
 close.addEventListener('click', () => {
   mobileMenuToggle();
+});
+
+const hearts = document.querySelectorAll('.heart');
+
+hearts.forEach(function (heart) {
+  heart.addEventListener('click', function () {
+    this.classList.toggle('clicked');
+  });
+});
+
+//SWIPER
+var swiper = new Swiper('.mySwiper', {
+  pagination: {
+    el: '.swiper-pagination',
+    clickable: true,
+    renderBullet: function (index, className) {
+      return '<span class="' + className + '">' + '</span>';
+    },
+  },
 });
